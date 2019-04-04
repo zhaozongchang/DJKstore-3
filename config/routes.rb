3c3bd3e710d6929f6f6f19bc72053d5ab226d425
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-04 11:49:35 +0800

    订单付款功能

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end 
 
   resources :cart_items
 
@@ -25,6 +30,6 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :orders
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
