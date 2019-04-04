849dcd3f6846a82139a7e4e0c34d525cda026cf3
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-04 14:21:42 +0800

    add notify_cancel/ship function

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
@@ -33,7 +34,7 @@ Rails.application.routes.draw do
         post :shipped
         post :return
       end
-    end 
+    end
   end
 
   namespace :account do
